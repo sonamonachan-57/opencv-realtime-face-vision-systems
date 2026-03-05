e3a3b3271fa7723b54d9c1d037b891c0df093df6
sonamonachan-57 <sonamonachan57@gmail.com>
2026-03-05 10:22:55 +0530

    Add files via upload

---
+++ drowsiness.py
@@ -0,0 +1,116 @@
+import os
+import sys
+
+venv_path = '/home/sona-inc5619/deep_env/lib/python3.12/site-packages'
+if os.path.exists(venv_path) and venv_path not in sys.path:
+    sys.path.insert(0, venv_path)
+    
+    
+   
+
+venv_path = '/home/sona-inc5619/mp_env/lib/python3.12/site-packages'
+if os.path.exists(venv_path) and venv_path not in sys.path:
+    sys.path.insert(0, venv_path)
+
+
+
+
+
+import cv2
+import dlib
+import pyttsx3
+from scipy.spatial import distance
+
+# for alert message
+engine = pyttsx3.init()
+
+
+cap = cv2.VideoCapture(0)
+
+# face detection
+
+face_detector = dlib.get_frontal_face_detector()
+
+# .dat file location
+
+dlib_facelandmark = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+# calcuating the aspect ration using eucliean distance
+
+
+def Detect_Eye(eye):
+    poi_A = distance.euclidean(eye[1], eye[5])
+    poi_B = distance.euclidean(eye[2], eye[4])
+    poi_C = distance.euclidean(eye[0], eye[3])
+    aspect_ratio_Eye = (poi_A+poi_B)/(2*poi_C)
+    return aspect_ratio_Eye
+
+
+
+while True:
+    null, frame = cap.read()
+    gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_detector(gray_scale)
+
+    for face in faces:
+        face_landmarks = dlib_facelandmark(gray_scale, face)
+        leftEye = [] 
+        rightEye = [] 
+
+        # left eyes in .dat file that are from 42 TO 47
+        for n in range(42, 48):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            rightEye.append((x, y))
+            next_point = n+1
+            if n == 47:
+                next_point = 42
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame, (x, y), (x2, y2), (0, 255, 0), 1)
+
+        
+        # Right eyes in .dat file that are from 36 TO 41
+        for n in range(36, 42):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            leftEye.append((x, y))
+            next_point = n+1
+            if n == 41:
+                next_point = 36
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame, (x, y), (x2, y2), (255, 255, 0), 1)
+
+        # calculating the aspect ratio for left and right eye
+        
+        right_Eye = Detect_Eye(rightEye)
+        left_Eye = Detect_Eye(leftEye)
+        Eye_Rat = (left_Eye+right_Eye)/2
+
+        #  round of the avarage mean value of left and right eye
+        
+        Eye_Rat = round(Eye_Rat, 2)
+
+        # This value  0.25 , will decide whether the person's eyes are close or not
+        
+        
+        if Eye_Rat < 0.25:
+            cv2.putText(frame, "DROWSINESS DETECTED", (50, 100),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (21, 56, 210), 3)
+            cv2.putText(frame, "Alert!!!! WAKE UP DUDE", (50, 450),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (21, 56, 212), 3)
+
+           # audio function and alert 
+           
+           
+            engine.say("Alert!!!! WAKE UP DUDE")
+            engine.runAndWait()
+
+    cv2.imshow("Drowsiness detection", frame)
+    key = cv2.waitKey(9)
+    if key == 20:
+        break
+cap.release()
+cv2.destroyAllWindows()
